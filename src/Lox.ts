3c17945e65f41f4f3fbd595382848d26c38cd71d
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-07-16 20:33:54 +0530

    Removed Out folder from git

--- src/Lox.ts
+++ src/Lox.ts
@@ -1,1 +1,16 @@
 class Lox {}
+
+// Runs only is the current module is called
+if (require.main === module) {
+    // Lox Class Instance
+    const lox = new Lox();
+
+    const args: string[] = process.argv;
+
+    console.log(args);
+
+    if (args.length > 1) {
+        console.log("Hey there!!!");
+    } else if (args.length === 1) {
+    }
+}
